5a8e8fd2e18bf4034583dbb00e500839765821d9
August Sodora <augsod@gmail.com>
2012-07-31 02:55:07 -0400

    UNOIDL: Make sure service members of services are public
    Also make sure namespace-qualified service members are scanned
    correctly

--- src/doxygen.cpp
+++ src/doxygen.cpp
@@ -2134,8 +2134,9 @@ static MemberDef *addVariableToClass(
   QCString def;
   if (!root->type.isEmpty())
   {
-    if (root->type.findRev("interface")!=-1 && rootNav->parent() &&
-        rootNav->parent()->type()=="service")
+    if ((root->type.findRev("interface")!=-1 ||
+         root->type.findRev("service")!=-1) &&
+        rootNav->parent() && rootNav->parent()->type()=="service")
     {
       // For interfaces in UNO IDL-style services
       def=root->type+" "+name;
@@ -2732,8 +2733,8 @@ static void addVariable(EntryNav *rootNav,int isFuncPtr=-1)
                              );
          }
       }
-      else if (root->type.findRev("interface")!=-1 && rootNav->parent() &&
-               rootNav->parent()->type()=="service")
+      else if ((root->type.findRev("interface")!=-1 || root->type.findRev("service")!=-1) &&
+               rootNav->parent() && rootNav->parent()->type()=="service")
       {
         // For interfaces in UNO IDL-style services
         cd=getClass(scope);
@@ -2908,8 +2909,8 @@ static void buildVarList(EntryNav *rootNav)
   {
     addVariable(rootNav,isFuncPtr);
   }
-  else if (rootNav->parent() && rootNav->parent()->type() == "service" &&
-           rootNav->type() == "interface")
+  else if ((rootNav->type().findRev("interface")!=-1 || rootNav->type().findRev("service")!=-1) &&
+           rootNav->parent() && rootNav->parent()->type() == "service")
   {
     // For interfaces in UNO IDL-style services
     addVariable(rootNav,isFuncPtr);
@@ -6572,8 +6573,8 @@ static void filterMemberDocumentation(EntryNav *rootNav)
     isFunc=FALSE;
   }
 
-  if (rootNav->parent() && rootNav->parent()->type() == "service" &&
-      root->type == "interface")
+  if ((root->type.findRev("interface")!=-1 || root->type.findRev("service")!=-1) &&
+      rootNav->parent() && rootNav->parent()->type()=="service")
   {
     // For UNO IDL-style interfaces
     findMember(rootNav,
